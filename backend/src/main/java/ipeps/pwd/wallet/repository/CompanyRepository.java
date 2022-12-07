e226bc8ebfd4e66b5f3dd8ad0d9980bc2080d5d2
stymax <grilli.frederic@hotmail.com>
2022-12-07 03:59:20 +0100

    Delete backend/src/main/java/ipeps/pwd/wallet/repository directory

--- backend/src/main/java/ipeps/pwd/wallet/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package ipeps.pwd.wallet.repository;
-
-import ipeps.pwd.wallet.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CompanyRepository extends JpaRepository<Company, UUID> {
-}
--- backend/src/main/java/ipeps/pwd/wallet/repository/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package ipeps.pwd.wallet.repository;
-
-import ipeps.pwd.wallet.entity.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrganizationRepository extends JpaRepository<Organization, UUID> {
-}
