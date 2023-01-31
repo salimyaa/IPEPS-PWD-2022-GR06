0da71fe716b2edb94cba32fa4c4ad0e09a347dac
stymax <grilli.frederic@hotmail.com>
2023-01-31 04:27:10 +0100

    Add files via upload

--- WalletApp/src/app/modules/employee/helper/employee.helper.ts
+++ WalletApp/src/app/modules/employee/helper/employee.helper.ts
@@ -4,11 +4,11 @@ import {CompanyHelper} from "../../company/helper/company.helper";
 import {SpecificConfigImage} from "@shared/module/list-generic/model/specific.config.image";
 import {CssForValue} from "@shared/module/list-generic/model/css-for-value.config";
 import {isNil} from "lodash";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 export class EmployeeHelper {
   public static getImageConfig(employee:Employee):SpecificConfigImage{
-   const src = (isNil(employee.picture))? employee.picture : (employee.gender === 'male')? 'url de ta photo male': 'url de ta pjoto femme';
+   const src = (isNil(employee.picture))? employee.picture : (employee.gender === 'male')? '../../../../assets/images/employee/men.png': 'url de ta pjoto femme';
     return {
       src: src,
       cssConfig: []
@@ -46,11 +46,11 @@ export class EmployeeHelper {
       phone:'',
       city:'',picture:''};
   }
-  static convertToUpdateForm(employee: Employee): FormGroup {
+  static convertToCreateForm(employee: Employee): FormGroup {
     return new FormGroup({
-      firstname: new FormControl(employee.firstname),
-        lastname: new FormControl(employee.lastname),
-        status: new FormControl(employee.status),
+      firstname: new FormControl(employee.firstname, [Validators.required]),
+        lastname: new FormControl(employee.lastname, [Validators.required]),
+        status: new FormControl(employee.status,[Validators.required]),
         city: new FormControl(employee.city),
         address: new FormControl(employee.address),
         phone: new FormControl(employee.phone),
@@ -58,7 +58,26 @@ export class EmployeeHelper {
         birthday: new FormControl(employee.birthday),
         gender: new FormControl(employee.gender),
         ssin: new FormControl(employee.ssin),
-        //company: new FormControl(employee.company),
+        company: new FormControl(employee.company.id),
+      picture: new FormControl(employee.picture),
+      //  active: new FormControl(employee.active),
+    });
+  }
+  static convertToUpdateForm(employee: Employee): FormGroup {
+    return new FormGroup({
+      employee_id: new FormControl(employee.id),
+      firstname: new FormControl(employee.firstname, [Validators.required]),
+      lastname: new FormControl(employee.lastname, [Validators.required]),
+      status: new FormControl(employee.status,[Validators.required]),
+      city: new FormControl(employee.city),
+      address: new FormControl(employee.address),
+      phone: new FormControl(employee.phone),
+      email: new FormControl(employee.email),
+      birthday: new FormControl(employee.birthday),
+      gender: new FormControl(employee.gender),
+      ssin: new FormControl(employee.ssin),
+      company: new FormControl(employee.company.id),
+      picture: new FormControl(employee.picture),
       //  active: new FormControl(employee.active),
     });
   }
